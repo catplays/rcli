ff0882ef327e5733ba780c6af4aa598770b1ea62
catwang <865469028@qq.com>
2024-05-19 16:52:19 +0800

    feat: http with index

--- src/process/http_serve.rs
+++ src/process/http_serve.rs
@@ -1,12 +1,12 @@
 use std::net::SocketAddr;
 use std::path::{PathBuf};
 use std::sync::Arc;
-use axum::{
-    extract::{Path, State},
-    http::StatusCode,
-};
+use axum::{extract::{Path, State}, http::StatusCode, Json};
+use axum::body::Body;
+use axum::response::Response;
 use axum::Router;
-use axum::routing::get;
+use axum::routing::{get, get_service};
+use tower_http::services::ServeDir;
 use tracing::{info, warn};
 
 #[derive(Debug)]
@@ -21,8 +21,12 @@ pub async fn process_http_serve(path: PathBuf, port: u16) -> anyhow::Result<()> 
     let listener = tokio::net::TcpListener::bind(addr)
         .await?;
 
-    let state = HttpServeState { path };
-    let router = Router::new().route("/*path", get(file_handler))
+    let state = HttpServeState { path: path.clone() };
+    let router = Router::new()
+        // /*path 只能匹配/a、/b/a/ 不能匹配到"/"。 所以对于/要不使用路由注册，或者匹配不上fallback。
+        .route("/v1/*path", get(file_handler))
+        .fallback(index_page)
+        .nest_service("/tower", ServeDir::new(path))
         .with_state(Arc::new(state));
 
 
@@ -30,13 +34,42 @@ pub async fn process_http_serve(path: PathBuf, port: u16) -> anyhow::Result<()> 
     Ok(())
 }
 
+
+async fn index_page(State(state): State<Arc<HttpServeState>>) -> Response{
+    let root = std::path::Path::new(&state.path);
+    let mut html_rsp:Vec<String> = Vec::new();
+    html_rsp.push(String::from("<html><body> <h1>目录</h1>"));
+    if root.is_dir() {
+        let paths = root.read_dir().unwrap();
+        for p in paths {
+            let entry = p.unwrap();
+            if entry.file_type().unwrap().is_file() {
+                println!("file:{}", entry.file_name().to_str().unwrap());
+                html_rsp.push(String::from("<p>"));
+                html_rsp.push(entry.file_name().into_string().unwrap());
+                html_rsp.push(String::from("</p>"));
+            }
+        }
+    }
+    html_rsp.push(String::from("</body> </html>"));
+    let html = html_rsp.join("\n");
+    Response::builder()
+        .status(StatusCode::OK)
+        .header("Content-Type", "text/html; charset=utf-8")
+        .body(Body::from(html))
+        .unwrap()
+}
+
 /**
-index_handler 中的参数都是实现了struct State<S>(pub S);的格式
+file_handler 中的参数都是实现了struct State<S>(pub S);的格式
  */
 async fn file_handler(State(state): State<Arc<HttpServeState>>,
                       Path(path): Path<String>) -> (StatusCode, String) {
+
+
     let p = std::path::Path::new(&state.path).join(path);
-    format!("Reading file {:?}", p);
+    info!("Reading file {:?}", p);
+    println!("Reading file {:?}", p);
     if !p.exists() {
         (StatusCode::NOT_FOUND,
          format!("File {} note found", p.display()),
@@ -52,5 +85,24 @@ async fn file_handler(State(state): State<Arc<HttpServeState>>,
                 (StatusCode::INTERNAL_SERVER_ERROR, e.to_string())
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use std::path::PathBuf;
+    use std::sync::Arc;
+    use axum::extract::{Path, State};
+    use axum::http::StatusCode;
+    use crate::process::http_serve::{file_handler, HttpServeState};
+
+    async fn test_file_handler() {
+        let state = Arc::new(HttpServeState {
+            path: PathBuf::from(".")
+        });
+
+        let (status, content) = file_handler(State(state), Path("Cargo.toml".to_string())).await;
+        assert_eq!(status, StatusCode::OK);
+        assert!(content.trim().starts_with("[package]"))
     }
 }
